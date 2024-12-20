6c4bb99ec541a9e40ea3dd5951eb1e1503ce4761
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2024-12-20 21:43:48 +0530

    theme toggle added

--- overseas/app/layout.tsx
+++ overseas/app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,11 +28,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(`${geistSans.variable} ${geistMono.variable} antialiased`, " bg-white dark:bg-[#313338]")}
       >
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem={false}
+        storageKey="discord-theme">
         {children}
+        </ThemeProvider>
       </body>
     </html>
     </ClerkProvider>
