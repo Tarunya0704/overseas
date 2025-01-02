8972cde92243026e63cf2f5b09523575c0f66934
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-01-02 21:34:30 +0530

    error resolved

--- overseas/lib/current-profile.ts
+++ overseas/lib/current-profile.ts
@@ -1,9 +1,33 @@
+// import { auth } from "@clerk/nextjs/server";
+// import { db } from "@/lib/db";
+
+// export const currentProfile = async () => {
+//     try {
+//         const { userId } = auth();
+
+//         if (!userId) {
+//             return null;
+//         }
+
+//         const profile = await db.profile.findUnique({
+//             where: {
+//                 userId: userId
+//             }
+//         });
+
+//         return profile;
+//     } catch (error) {
+//         console.error("Error fetching profile:", error);
+//         return null;
+//     }
+// }
+
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
 
 export const currentProfile = async () => {
     try {
-        const { userId } = auth();
+        const { userId } = await auth();
 
         if (!userId) {
             return null;
@@ -11,7 +35,7 @@ export const currentProfile = async () => {
 
         const profile = await db.profile.findUnique({
             where: {
-                userId: userId
+                userId
             }
         });
 
