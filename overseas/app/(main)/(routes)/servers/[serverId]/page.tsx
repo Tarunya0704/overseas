01a57c58fdad76d23b55e4291ac172f432d7039d
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-01-06 16:10:13 +0530

    sidbar started

---
+++ overseas/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,7 @@
+const ServerIdPage = () => {
+    return ( 
+        <div>this is server id page</div>
+     );
+}
+ 
+export default ServerIdPage;
--- overseas/lib/current-profile.ts
+++ overseas/lib/current-profile.ts
@@ -1,26 +1,4 @@
-// import { auth } from "@clerk/nextjs/server";
-// import { db } from "@/lib/db";
 
-// export const currentProfile = async () => {
-//     try {
-//         const { userId } = auth();
-
-//         if (!userId) {
-//             return null;
-//         }
-
-//         const profile = await db.profile.findUnique({
-//             where: {
-//                 userId: userId
-//             }
-//         });
-
-//         return profile;
-//     } catch (error) {
-//         console.error("Error fetching profile:", error);
-//         return null;
-//     }
-// }
 
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/db";
