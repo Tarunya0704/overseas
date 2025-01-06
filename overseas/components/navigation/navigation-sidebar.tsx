85eb412240cb0e9f6d931aa8ed1a5c190ab6af57
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-01-06 20:56:56 +0530

    sidebar

--- overseas/components/navigation/navigation-sidebar.tsx
+++ overseas/components/navigation/navigation-sidebar.tsx
@@ -1,6 +1,7 @@
 import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
+import { NavigationAction } from "./navigation-action";
 
  export const NavigationSidebar =  async() => {
     const profile = await currentProfile();
@@ -9,13 +10,21 @@ import { db } from "@/lib/db";
         return redirect("/");
     }
 
-    const servers = await db.server.findMany({})
+    const servers = await db.server.findMany({
+        where:{
+            member:{
+                some: {
+                    profileId:profile.id
+                }
+            }
+        }
+    });
 
 
 
     return ( 
-        <div>
-            this is navigation
+        <div className=" space-y-4 flex felx-col items-center h-full text-primary w-full dark:bg-[#1E1F22]">
+            <NavigationAction/>
         </div>
      );
 }
