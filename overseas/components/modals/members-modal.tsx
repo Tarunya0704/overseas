1317d72d5a8b0498097ba88fc639032078089eb3
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-02-05 18:10:51 +0530

    member role updation and kick thing is working

--- overseas/components/modals/members-modal.tsx
+++ overseas/components/modals/members-modal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-
-
+import axios from "axios";
+import qs from "query-string"
 import {
     Dialog,
     DialogContent,
@@ -13,13 +13,49 @@ import {
     
   } from '@/components/ui/dialog'
 
+  import {
+    DropdownMenu ,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuSeparator,
+    
+    DropdownMenuPortal,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+   
+
+
+
+  } from "@/components/ui/dropdown-menu";
+
 
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMemebrsWithProfiles } from "@/types";
 import { ScrollArea } from '../ui/scroll-area';
+import { UserAvatar } from '../user-avatar';
+import { Check, Gavel, Loader2, MoreVertical, Shield, ShieldAlert, ShieldCheck, ShieldQuestion } from 'lucide-react';
+import { useState } from 'react';
+import { MemberRole } from '@prisma/client';
+import { useRouter } from "next/navigation";
+const roleIconMap = {
+    "GUEST": null,
+    "MODERATOR": <ShieldCheck className='h-4 w-4 ml-2 text-indigo-500'/>,
+    "ADMIN":<ShieldAlert className='h-4 w-4 text-rose-500'/>
+
+
+}
+
 
 export const MembersModal = () => {
+
+    const router = useRouter();
+
+
     const { onOpen ,isOpen , onClose, type , data} = useModal();
+    const [loadingId, setLoadingId] = useState("");
+    
     
 
 
@@ -27,6 +63,49 @@ export const MembersModal = () => {
 
     const isModalOpen = isOpen && type === "members";
     const {server} = data as { server: ServerWithMemebrsWithProfiles};
+
+    const onKick = async (memberId: string) => {
+        try{
+            setLoadingId(memberId);
+            const url = qs.stringifyUrl({
+                url: `/api/members/${memberId}`,
+                query: {
+                    serverId: server?.id,
+                }
+            });
+            const response = await axios.delete(url);
+            router.refresh();
+            onOpen("members", {server : response.data});
+
+        }catch (error) {
+            console.error(error);
+        }finally {
+            setLoadingId("");
+        }
+    }
+
+
+    const onRoleChange = async (role: MemberRole, memberId: string) => {
+        try {
+            setLoadingId(memberId);
+            const url = qs.stringifyUrl({
+                url: `/api/members/${memberId}`,
+                query: {
+                    serverId: server?.id,
+                    
+                }
+            });
+            const response  = await axios.patch(url, {role});
+            router.refresh();
+           onOpen("members", {server : response.data});
+        }
+        catch (error) {
+            console.error(error);
+            
+        }finally {
+            setLoadingId("");
+        }
+    }
 
     
   
@@ -44,7 +123,7 @@ export const MembersModal = () => {
 return (
 
     <Dialog open={isModalOpen} onOpenChange={onClose}>
-        <DialogContent className='bg-white text-black p-0 overflow-hidden'>
+        <DialogContent className='bg-white text-black  overflow-hidden'>
             <DialogHeader className='pt-8 px-6 '>
                 <DialogTitle className='text-center text-2xl font-bold'>
                     Manage Members
@@ -59,10 +138,72 @@ return (
             <ScrollArea className='mt-8 max-h-[420px] pr-6'>
                 {server?.member?.map((member) => (
                     <div key={member.id} className='flex items-center gap-x-2 mb-6 '>
+                        <UserAvatar src={member.profile.imageUrl}/>
+
+                        <div className='flex flex-col gap-y-1'>
+                            <div className='text-xs  fort-semibold flex items-center gap-x-1'>
+                                {member.profile.name}
+                                {roleIconMap[member.role]}
+
+                            </div>
+                            <p className='text-xs text-zinc-500'>
+                                {member.profile.email}
+                            </p>
+
+                        </div>
+                        {server.profileId !== member.profile.id &&  loadingId !== member.id && loadingId !== member.id &&(
+                            <div className='ml-auto'>
+                                <DropdownMenu>
+                                    <DropdownMenuTrigger>
+                                        <MoreVertical className='h-4 w-4 text-zinc-500'/>
+                                    </DropdownMenuTrigger>
+                                    <DropdownMenuContent side='left'>
+                                        <DropdownMenuSub>
+                                            <DropdownMenuSubTrigger 
+                                            className='flex items-center '>
+                                                <ShieldQuestion  className='w-4 h-4 mr-2'/>
+                                                <span>Role</span>
+
+                                            </DropdownMenuSubTrigger>
+
+                                            <DropdownMenuPortal>
+                                                <DropdownMenuSubContent>
+                                                    <DropdownMenuItem onClick={() => onRoleChange("GUEST", member.id)}>
+                                                        <Shield className='w-4 h-4 mr-2'/>
+                                                        Guest
+                                                        {member.role === "GUEST" && (
+                                                            <Check className='w-4 h-4 ml-auto'/>
+                                                        )}
+                                                    </DropdownMenuItem>
+                                                    <DropdownMenuItem onClick={() => onRoleChange( "MODERATOR",member.id)}>
+                                                        <ShieldCheck className='w-4 h-4 mr-2'/>
+                                                        Moderator
+                                                        {member.role === "MODERATOR" && (
+                                                            <Check className='w-4 h-4 ml-auto'/>
+                                                        )}
+                                                    </DropdownMenuItem>
+                                                </DropdownMenuSubContent>
+                                            </DropdownMenuPortal>
+                                        </DropdownMenuSub>
+                                        <DropdownMenuSeparator/>
+                                        <DropdownMenuItem onClick={() => onKick(member.id)}>
+                                            <Gavel className='h-4 w-4 mr-2'/>
+                                            Kick
+                                        </DropdownMenuItem>
+
+                                    </DropdownMenuContent>
+                                </DropdownMenu>
+                            </div>
+
+                                                    )}
+                            {loadingId === member.id && (
+                                <Loader2 className='animate-spin text-zinc-500 h-4 w-4 ml-auto'/>
+                        
+
+                        )}
 
                     </div>
-                ))
-            }
+                ))}
 
             </ScrollArea>
            
