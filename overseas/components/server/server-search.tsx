d3591476fd66739a9ab304b0a34f8713835d49a0
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-03-17 06:09:37 +0530

    search server

--- overseas/components/server/server-search.tsx
+++ overseas/components/server/server-search.tsx
@@ -1,5 +1,6 @@
 "use client ";
 
+import { Search } from "lucide-react";
 import React from "react";
 
 
@@ -22,7 +23,21 @@ export const ServerSearch = ({
 }: ServerSearchProps) => {
     return (
         <div>
-            server search component 
+           <>
+           <button className="group px-2 py-2 rounded-md flex items-center gap=-x-2  w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+           <Search className="w-4 h-4 text-zinc-500 dark:test-zinc-400" />
+            <p className=" font-semibold text-sm  text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+
+                Search server
+            </p>
+            <kbd>
+                <span className=" pointer-events-none inline-flex h-5 select-none items-center  text-xs font-semibold text-zinc-500 dark:text-zinc-400 group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition">
+                    CMD
+                </span>K
+                
+            </kbd>
+           </button>
+           </> 
         </div>
     )
 
