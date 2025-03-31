947055dfed64fc78df88ffc4def9d8dbafe11363
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-03-31 10:54:55 +0530

    chat header addded

--- overseas/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ overseas/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,11 +1,64 @@
+import { currentProfile } from "@/lib/current-profile";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ChatHeader } from "@/components/chat/chat-header";
+
+import { db } from "@/lib/db";
 
 
-const   channelIdpage = () => {
+interface ChannelIdPageProps {
+  params:{
+    serverId: string;
+    channelId: string;
+  }
+}
+
+
+const   ChannelIdPage = async ({
+  params
+}: ChannelIdPageProps) => {
+
+  const profile = await currentProfile();
+
+  if(!profile){
+    return RedirectToSignIn();
+  }
+
+
+const channel = await db.channel.findUnique({
+  where:{
+    id:params.channelId,
+  },
+});
+
+const member = await db.member.findFirst({
+  where:{
+    serverId: params.serverId,
+    profileId: profile.id,
+  },
+});
+
+
+if(!channel || !member) {
+  redirect("/");
+}
+
+
+
+
+
+
   return (
-    <div>
-      channel id page
+    <div className="bg-white flex flex-col h-full dark:bg-[#313338]">
+      <ChatHeader
+      name={channel.name}
+      serverId={channel.serverId}
+      type="channel"
+
+      />
+      
     </div>
   );
 }
 
-export default channelIdpage;
+export default ChannelIdPage;
