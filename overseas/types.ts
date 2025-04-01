17660be649f2f3a9c2f4ce2977655ce087c05663
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-04-01 19:48:54 +0530

    socket.io  setup started

--- overseas/types.ts
+++ overseas/types.ts
@@ -1,6 +1,20 @@
+
+
+import {Server as NetServer ,  Socket} from "net";
+import { NextApiResponse } from "next";
+import {Server as SocketIOServer } from "socket.io";
 import { Profile } from "@prisma/client"
 import { Server , Member } from "@prisma/client"
+
 
 export type ServerWithMemebrsWithProfiles = Server & {
     member:(Member & {profile:Profile}) [];
 };
+
+export type NextApiResponseServerIo = NextApiResponse & {
+    socket: Socket & {
+        server: NetServer & {
+            io:SocketIOServer;
+        };
+    };
+}
