582ebc65c02fd102e2c24ba8243dc3486eb2469b
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-04-09 22:33:51 +0530

    pdf upload feature added but its not working idk why ?

--- overseas/components/file-upload.tsx
+++ overseas/components/file-upload.tsx
@@ -87,6 +87,10 @@ import { X } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 import "@uploadthing/react/styles.css";
+import { FileIcon } from "lucide-react";
+
+
+
 
 interface FileUploadProps {
     onChange: (url?: string) => void;
@@ -131,6 +135,29 @@ export const FileUpload = ({
             </div>
         )
     }
+    if(value  && fileType === "pdf"){
+        return (
+            <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                <FileIcon className = "h-10 w-10 fill-indigo-200 stroke-indigo-400"/>
+                <a
+                href={value}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-sm text-indigo-500 hover:underline ml-2 dark:text-indigo-400">
+                    {value}
+
+                </a>
+                <button onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2   -right-2 shadow-sm " type="button">
+                    <X className="h-4 w-4"/>
+                </button>
+
+            </div>
+        )
+    }
+
+
+
 
     return (
         <UploadDropzone
