8ba470ba1b57688b2c0092843b8ab027bd632875
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-04-11 03:40:20 +0530

    chat query (sleepy)

--- overseas/app/layout.tsx
+++ overseas/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,7 +45,9 @@ export default function RootLayout({
         >
           <SocketProvider>
           <ModalProvider/>
+          <QueryProvider>
           {children}
+          </QueryProvider>
           </SocketProvider>
         
         </ThemeProvider>
