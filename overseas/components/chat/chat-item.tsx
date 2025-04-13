2474b2e018a5d28bd988449851ad0f47be136f65
Tarunya0704 <115094431+Tarunya0704@users.noreply.github.com>
2025-04-13 14:01:52 +0530

    working on chat component

--- overseas/components/chat/chat-item.tsx
+++ overseas/components/chat/chat-item.tsx
@@ -1,9 +1,13 @@
 "use client ";
 
-import { Member, Message, Profile } from "@prisma/client";
+import { Member, MemberRole, Message, Profile } from "@prisma/client";
 import { UserAvatar } from "../user-avatar";
 import { ActionTooltip } from "../action-tooltip";
-import { ShieldAlert, ShieldCheck } from "lucide-react";
+import { Edit, ShieldAlert, ShieldCheck } from "lucide-react";
+import Image from "next/image";
+import { FileIcon } from "lucide-react";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 interface ChatItemProps {
     id: string;
@@ -43,6 +47,24 @@ export const  ChatItem = ({
     socketUrl,
     socketQuery
 }: ChatItemProps) => {
+    const [isEditing , setIsEditing] = useState(false);
+    const [isDeleting , setIsDeleting] = useState(false);
+
+
+    const fileType = fileUrl?.split(".").pop() ;
+
+
+
+    const isAdmin = currentMember.role === MemberRole.ADMIN;
+    const isModerator = currentMember.role === MemberRole.MODERATOR;
+    const isOwner =  currentMember.id === member.id;
+    const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+    const canEditMessage = !deleted &&  isOwner && !fileUrl;
+    const isPDF = fileType === "pdf" && fileUrl;
+    const isImage = !isPDF && fileUrl;
+    
+
+
     return (
         <div className="relative group flex  items-center hover:bg-black/5 p-4 transition w-full ">
             <div className="group flex gap-x-2 items-start w-full">
@@ -67,11 +89,65 @@ export const  ChatItem = ({
                         </span>
 
                     </div>
-                    {content}
+                    {isImage && (
+                        <a href={fileUrl} target="_blank" rel="noreferrer" 
+                        className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48">
+                        <Image
+                        src={fileUrl}
+                        alt=""
+                        fill
+                        className="object-cover "
+                        />
+                        
+                        </a>
+                    )}
+
+                    {isPDF && (
+                        <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+                        <FileIcon className = "h-10 w-10 fill-indigo-200 stroke-indigo-400"/>
+                        <a
+                        href={fileUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-sm text-indigo-500 hover:underline ml-2 dark:text-indigo-400">
+                            pdf file
+                        </a>
+                       
+                        
+                        </div>
+                         
+
+                    )}
+                    {!fileUrl &&  !isEditing && (
+                        <p className={cn("text-sm text-zinc-600 dark:text-zinc-300",
+                            deleted && "italic text-zinc-500 dark:text-zinc-400 text-xs mt-1"
+                        )}>
+                            {content}
+                            {isUpdated && !deleted && (
+                                <span className="text-[10px] text-zinc-500 dark:text-zinc-400 mx-2">
+                                    (edited)
+                                </span>
+                            )}
+                        </p>
+                    )}
 
                 </div>
-
             </div>
+            {canDeleteMessage && (
+                <div className="hidden group-hover:flex absolute items-center gap-x-2 p-1 -top-2 right-5 bg-white dark:bg-zinc-800  border rounded-sm ">
+                    {canEditMessage && (
+                        <ActionTooltip label="Edit">
+                            <Edit
+                            className="cursor-pointer h-4 w-4 ml-auto  text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-200 transition"
+                            />
+
+                        </ActionTooltip>
+                    )}
+
+                </div>
+            )}
+
+
         </div>
     )
 }
